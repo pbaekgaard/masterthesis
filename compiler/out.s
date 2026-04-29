21d10730d963c2b6121734554840da2db702f304
pbaekgaard <pbakga21@student.aau.dk>
2026-04-29 09:52:34 +0200

    refactor fault injection and better memory model

---
+++ compiler/out.s
@@ -0,0 +1,125 @@
+.syntax unified
+.thumb
+
+.section .text
+.global _start
+.type _start, %function
+
+_start:
+    sub sp, sp, #4
+    mov r0, #0
+    str r0, [sp]
+    sub sp, sp, #4
+    mov r0, #1
+    str r0, [sp]
+    sub sp, sp, #4
+    mov r0, #2
+    str r0, [sp]
+    sub sp, sp, #4
+    mov r0, #3
+    str r0, [sp]
+    sub sp, sp, #4
+    mov r0, #4
+    str r0, [sp]
+    sub sp, sp, #4
+    mov r0, #0
+    str r0, [sp]
+    sub sp, sp, #4
+    mov r0, #0
+    str r0, [sp]
+    sub sp, sp, #4
+    mov r0, #0
+    str r0, [sp]
+    sub sp, sp, #4
+    mov r0, #0
+    str r0, [sp]
+    sub sp, sp, #4
+    mov r0, #0
+    str r0, [sp]
+    ldr r0, [sp, #16]
+    mov r1, r0
+    ldr r0, [sp, #32]
+    cmp r1, r0
+    mov r0, #0
+    it eq
+    moveq r0, #1
+    cmp r0, #0
+    beq else_0
+    ldr r0, [sp, #0]
+    mov r1, r0
+    mov r0, #1
+    add r0, r1, r0
+    str r0, [sp]
+    b endif_0
+else_0:
+endif_0:
+    ldr r0, [sp, #12]
+    mov r1, r0
+    ldr r0, [sp, #28]
+    cmp r1, r0
+    mov r0, #0
+    it eq
+    moveq r0, #1
+    cmp r0, #0
+    beq else_1
+    ldr r0, [sp, #0]
+    mov r1, r0
+    mov r0, #1
+    add r0, r1, r0
+    str r0, [sp]
+    b endif_1
+else_1:
+endif_1:
+    ldr r0, [sp, #8]
+    mov r1, r0
+    ldr r0, [sp, #24]
+    cmp r1, r0
+    mov r0, #0
+    it eq
+    moveq r0, #1
+    cmp r0, #0
+    beq else_2
+    ldr r0, [sp, #0]
+    mov r1, r0
+    mov r0, #1
+    add r0, r1, r0
+    str r0, [sp]
+    b endif_2
+else_2:
+endif_2:
+    ldr r0, [sp, #4]
+    mov r1, r0
+    ldr r0, [sp, #20]
+    cmp r1, r0
+    mov r0, #0
+    it eq
+    moveq r0, #1
+    cmp r0, #0
+    beq else_3
+    ldr r0, [sp, #0]
+    mov r1, r0
+    mov r0, #1
+    add r0, r1, r0
+    str r0, [sp]
+    b endif_3
+else_3:
+endif_3:
+    ldr r0, [sp, #0]
+    mov r1, r0
+    mov r0, #4
+    cmp r1, r0
+    mov r0, #0
+    it eq
+    moveq r0, #1
+    cmp r0, #0
+    beq else_4
+    mov r0, #1
+    str r0, [sp, #36]
+    b endif_4
+else_4:
+endif_4:
+    ldr r0, [sp, #36]
+    mov r7, #1
+    svc #0
+
+.size _start, .-_start
